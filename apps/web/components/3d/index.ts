ddb24388ae48db30e46e36bd3d0561f86b652b61
Kemal Teksal <kemalteksal@192.168.1.41>
2026-01-11 01:43:04 +0300

    chore(web): cleanup old app/ and components/ structure
    V2 architecture uses src/ directory pattern.
    Old files moved/replaced with new structure.

--- apps/web/components/3d/index.ts
+++
@@ -1,2 +0,0 @@
-export { Scene3D } from './Scene3D';
-export { LovePortal3D, LovePortal2D } from './LovePortal3D';
--- apps/web/components/ceremony/index.ts
+++
@@ -1,4 +0,0 @@
-export { CeremonyShowcase } from './CeremonyShowcase';
-export { FeatureCard } from './FeatureCard';
-export { InteractiveDemo } from './InteractiveDemo';
-export { TrustBadgeDisplay } from './TrustBadgeDisplay';
